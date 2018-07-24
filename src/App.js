b823340d0784db2031402ffcf9ee028a8c9eab17
Rockydonthurtem <charrisq@hotmail.com>
2018-07-24 09:26:53 -0500

    drills

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: ""
+    }
+    this.handleMessage = this.handleMessage.bind(this)
+  }
+  handleMessage(val) {
+    this.setState({ messages: val })
+  }
+  render() {
+    return (
+      <div className="App">
+        <input type="text" onChange={(e) => this.handleMessage(e.target.value)} />
+        <p> {this.state.messages}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
